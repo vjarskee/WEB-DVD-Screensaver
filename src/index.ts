0e1362ffa36b63c8a136ecb213dfbfb564766e7c
VJ <71339544+vjarskee@users.noreply.github.com>
2024-02-20 02:05:03 +0300

    rewrite on typescript / vite

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,33 @@
 import './style.css'
 
-const colors = ['white', 'red', 'purple', 'fuchsia', 'green', 'lime', 'yellow', '	blue', 'teal', 'aqua']
+const colors: string[] = ['white', 'red', 'purple', 'fuchsia', 'green', 'lime', 'yellow', 'blue', 'teal', 'aqua']
 
-const logo = document.querySelector('.dvd_logo')
+const logo: HTMLDivElement = document.querySelector('.dvd_logo') as HTMLDivElement
 
-let right_border = window.innerWidth - 211
-let bottom_border = window.innerHeight - 101
-
-window.onresize = () => {
-  right_border = window.innerWidth - 211
-  bottom_border = window.innerHeight - 101
-}
+let right_border: number = window.innerWidth - 211
+let bottom_border: number = window.innerHeight - 101
 
 logo.style.top = bottom_border / 2 + 'px'
 logo.style.left = right_border / 2 + 'px'
 logo.style.fill = colors[Math.floor(Math.random() * colors.length)]
 
-let animation_interval = 5
+const animation_interval: number = 5
 
-logo.style.opacity = 1
+logo.style.opacity = '1'
 
-let state = {
+const state: Record<'x_direction' | 'y_direction', boolean> = {
   x_direction: Math.random() > 0.5,
   y_direction: Math.random() > 0.5
 }
 
-const setColor = () => {
-  let currentColor = logo.style.fill
+const setColor = (): void => {
+  const currentColor: string = logo.style.fill
   while (logo.style.fill === currentColor) {
     logo.style.fill = colors[Math.floor(Math.random() * colors.length)]
   }
 }
 
-let x_interval = setInterval(() => {
+setInterval((): void => {
   if (parseInt(logo.style.left) === right_border) {
     state.x_direction = false
     setColor()
@@ -45,7 +40,7 @@ let x_interval = setInterval(() => {
   if (!state.x_direction) logo.style.left = parseInt(logo.style.left) - 1 + 'px'
 }, animation_interval)
 
-let y_interval = setInterval(() => {
+setInterval((): void => {
   if (parseInt(logo.style.top) === bottom_border) {
     state.y_direction = false
     setColor()
@@ -57,3 +52,8 @@ let y_interval = setInterval(() => {
   if (state.y_direction) logo.style.top = parseInt(logo.style.top) + 1 + 'px'
   if (!state.y_direction) logo.style.top = parseInt(logo.style.top) - 1 + 'px'
 }, animation_interval)
+
+window.onresize = () => {
+  right_border = window.innerWidth - 211
+  bottom_border = window.innerHeight - 101
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(process.cwd(), 'src')
+    }
+  },
+
+  base: '/',
+
+  root: resolve(process.cwd(), 'src'),
+
+  build: {
+    outDir: resolve(process.cwd(), 'dist')
+  }
+})
